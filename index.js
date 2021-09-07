a3ec7288a073deea7adea0998f44739ac15c0062
adrianovaladares <adrianovaladares@as2group.com.br>
2021-09-06 23:25:25 -0300

    implementação dos asteroids

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const CANVASTAG = document.getElementById("game");
 const canvasContext = CANVASTAG.getContext("2d");
 var gameIsRunning = true;
-const FRAME = 1000 / 60;
+const numberOfAsteroids = 10;
 
 const square = new Square(15, 15, "green", 225, 225, canvasContext);
+const asteroidList = [];
+
+// for (let index = 0; index < numberOfAsteroids; index++) {
+//   const asteroid = new Asteroid(15, 15, "white", canvasContext);
+//   asteroid.setPositionAndDirection();
+//   asteroidList.push(asteroid);
+// }
+
+let time = new Date();
 
 function gameLoop() {
+  console.time();
   const loop = () => {
     render();
+    if (asteroidList.length < 10) {
+      const newTime = new Date();
+      if (time.getSeconds() !== newTime.getSeconds()) {
+        const asteroid = new Asteroid(15, 15, "white", canvasContext);
+        asteroid.setPositionAndDirection();
+        asteroidList.push(asteroid);
+        time = newTime;
+      }
+    }
     window.requestAnimationFrame(loop);
   };
   window.requestAnimationFrame(loop);
@@ -39,6 +58,7 @@ function clearScreen() {
 function Square(width, height, color, x, y, ctx) {
   this.width = width;
   this.height = height;
+  this.color = color;
   this.angle = 0;
   this.moveAngle = 0;
   this.speedX = 0;
@@ -47,7 +67,7 @@ function Square(width, height, color, x, y, ctx) {
   this.y = y;
   this.draw = () => {
     ctx.beginPath();
-    ctx.fillStyle = color;
+    ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
     ctx.closePath();
   };
@@ -89,9 +109,59 @@ function Square(width, height, color, x, y, ctx) {
   };
 }
 
+function Asteroid(width, height, color, ctx) {
+  this.width = width;
+  this.height = height;
+  this.color = color;
+  this.angle = 0;
+  this.moveAngle = 0;
+  this.speedX = 0.3;
+  this.speedY = 0.3;
+  this.x = 0;
+  this.y = 0;
+  this.draw = () => {
+    ctx.save();
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+    ctx.fillRect(this.width / 2, this.height / 2, this.width, this.height);
+    ctx.closePath();
+    ctx.restore();
+  };
+  this.move = () => {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    this.angle += (this.moveAngle * Math.PI) / 180;
+    if (this.x > CANVASTAG.width + this.width) {
+      this.x = -this.width;
+    } else if (this.x < -this.width) {
+      this.x = CANVASTAG.width + this.width;
+    }
+    if (this.y > CANVASTAG.height + this.height) {
+      this.y = -this.height;
+    } else if (this.y < -this.height) {
+      this.y = CANVASTAG.height + this.height;
+    }
+  };
+  this.setPositionAndDirection = () => {
+    const xDirection = Math.floor(Math.random() * 2);
+    const yDirection = Math.floor(Math.random() * 2);
+    this.moveAngle = Math.floor(Math.random() * 2);
+    this.x = Math.floor(Math.random() * CANVASTAG.width) * xDirection;
+    this.y = Math.floor(Math.random() * CANVASTAG.height) * yDirection;
+    this.speedX = xDirection ? this.speedX * -1 : this.speedX;
+    this.speedY = yDirection ? this.speedY * -1 : this.speedY;
+  };
+}
+
 function render() {
   clearScreen();
   square.draw();
+  asteroidList.forEach((asteroid) => {
+    asteroid.draw();
+    asteroid.move();
+  });
 }
 
 gameLoop();
