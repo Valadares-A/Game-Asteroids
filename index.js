10bb384fa2159e7290512ee080f87729d9e34402
adrianovaladares <adrianovaladares@as2group.com.br>
2021-09-08 22:21:18 -0300

    Squashed commit of the following:
    commit 1fafe9667b334f6e442f27745d9c6aaedc3efc10
    Author: adrianovaladares <adrianovaladares@as2group.com.br>
    Date:   Wed Sep 8 22:18:18 2021 -0300
    
        Merge de branchs e implementação de colisão
    
    commit a3ec7288a073deea7adea0998f44739ac15c0062
    Author: adrianovaladares <adrianovaladares@as2group.com.br>
    Date:   Mon Sep 6 23:25:25 2021 -0300
    
        implementação dos asteroids
    
    commit 1838966625a1f057639579e1ecefab705f219319
    Author: adrianovaladares <adrianovaladares@as2group.com.br>
    Date:   Sat Sep 4 23:50:55 2021 -0300
    
        Implementação simples de movimentação para quadrado
    
    commit 215de6810c11d88c496414fb4304729e9d63e175
    Author: adrianovaladares <adrianovaladares@as2group.com.br>
    Date:   Thu Sep 2 00:21:55 2021 -0300
    
        implementações de game teste para entender o funcionamento do canvas

--- index.js
+++ index.js
@@ -1,52 +1,226 @@
+console.log(window);
+const CANVASTAG = document.getElementById("game");
+const canvasContext = CANVASTAG.getContext("2d");
 
-var backgound = document.getElementById("game");
-var ctx = backgound.getContext("2d");
+const numberOfAsteroids = 10;
 
-const square = new component(30, 30, "red", 225, 225);
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false;
+var downPressed = false;
 
-function clearScreen(){
-    ctx.clearRect(0, 0, 500, 500)
+const spaceShip = new Square(15, 15, "green", 225, 225, canvasContext);
+const asteroidList = [];
+
+let time = new Date();
+
+function gameLoop() {
+  const loop = () => {
+    render();
+    if (asteroidList.length < numberOfAsteroids) {
+      const newTime = new Date();
+      if (time.getSeconds() !== newTime.getSeconds()) {
+        const asteroid = new Asteroid(15, 15, "white", canvasContext);
+        asteroid.setPositionAndDirection();
+        asteroidList.push(asteroid);
+        time = newTime;
+      }
+    }
+    window.requestAnimationFrame(loop);
+  };
+  window.requestAnimationFrame(loop);
 }
-function component(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.angle = 0;
-    this.speed = 1;
-    this.x = x;
-    this.y = y;
-    this.update = function() {
-      ctx.save();
-      ctx.translate(this.x, this.y);
-      ctx.rotate(this.angle);
-      ctx.fillStyle = color;
-      ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height);
-      ctx.restore();
-    }
-    
-    this.newPos = function() {
-      this.x += this.speed * Math.sin(this.angle);
-      this.y -= this.speed * Math.cos(this.angle);
-    }
+
+function inputProcess() {
+  document.addEventListener("keydown", keyDownHandler, false);
+  document.addEventListener("keyup", keyUpHandler, false);
 }
 
-// class seiLa{
-// 	constructor(x,y){}
+function clearScreen() {
+  canvasContext.clearRect(0, 0, 500, 500);
+}
 
-// 	newPos(){
-// 		this.x += this.speed * Math.sin(this.angle);
-//       		this.y -= this.speed * Math.cos(this.angle);
-// 	}
+function Square(width, height, color, x, y, ctx) {
+  this.width = width;
+  this.height = height;
+  this.color = color;
+  this.angle = 0;
+  this.moveAngle = 0;
+  this.speed = 0;
+  this.x = x;
+  this.y = y;
+  this.increaseSpeed = function () {
+    this.speed = this.speed + 0.15;
+  };
+  this.setSpeedToZero = function () {
+    if (this.speed > 0) {
+      this.speed = this.speed - 0.05;
+    } else if (this.speed < 0) {
+      this.speed = this.speed + 0.05;
+    } else {
+      this.speed = 0;
+    }
+  };
 
-// }
+  this.draw = function () {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height);
+    ctx.restore();
+  };
 
-// var kkkk = new seiLa(10,20)
-
-function updateGameArea() {
-	clearScreen();
-	square.newPos();
-	square.update();
+  this.update = function () {
+    if (rightPressed) {
+      this.moveAngle = 3;
+    } else if (leftPressed) {
+      this.moveAngle = -3;
+    } else {
+      this.moveAngle = 0;
+    }
+    if (upPressed) {
+      if (this.speed < 7) {
+        this.increaseSpeed();
+      }
+    } else if (downPressed) {
+      if (this.speed > -7) {
+        this.speed = this.speed - 0.15;
+      }
+    } else {
+      this.setSpeedToZero();
     }
 
-const gameLopp = setInterval(() => {
-	updateGameArea();
-}, 20);
+    this.angle += (this.moveAngle * Math.PI) / 180;
+    this.x += this.speed * Math.sin(this.angle);
+    this.y -= this.speed * Math.cos(this.angle);
+
+    if (this.x > CANVASTAG.width + this.width) {
+      this.x = 0;
+    }
+    if (this.x < -this.width) {
+      this.x = CANVASTAG.width + this.width;
+    }
+    if (this.y > CANVASTAG.height + this.height) {
+      this.y = 0;
+    }
+    if (this.y < -this.height) {
+      this.y = CANVASTAG.height + this.height;
+    }
+  };
+
+  this.checkCollisions = function (object) {
+    if (
+      this.x < object.x + object.width &&
+      this.x + this.width > object.x &&
+      this.y < object.y + object.height &&
+      this.y + this.height > object.y
+    ) {
+      console.log("bati");
+    }
+  };
+}
+
+function keyDownHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = true;
+  }
+  if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = true;
+  }
+  if (e.key == "Up" || e.key == "ArrowUp") {
+    upPressed = true;
+  }
+  if (e.key == "Down" || e.key == "ArrowDown") {
+    downPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = false;
+  }
+  if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = false;
+  }
+  if (e.key == "Up" || e.key == "ArrowUp") {
+    upPressed = false;
+  }
+  if (e.key == "Down" || e.key == "ArrowDown") {
+    downPressed = false;
+  }
+}
+
+function Asteroid(width, height, color, ctx) {
+  this.width = width;
+  this.height = height;
+  this.color = color;
+  this.angle = 0;
+  this.moveAngle = 0;
+  this.speedX = 0.3;
+  this.speedY = 0.3;
+  this.x = 0;
+  this.y = 0;
+  this.canRender = true;
+  this.draw = function () {
+    ctx.save();
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+    ctx.fillRect(this.width / 2, this.height / 2, this.width, this.height);
+    ctx.closePath();
+    ctx.restore();
+  };
+  this.move = function () {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    this.angle += (this.moveAngle * Math.PI) / 180;
+    if (this.x > CANVASTAG.width + this.width) {
+      this.x = 0;
+    } else if (this.x < -this.width) {
+      this.x = CANVASTAG.width + this.width;
+    }
+    if (this.y > CANVASTAG.height + this.height) {
+      this.y = 0;
+    } else if (this.y < -this.height) {
+      this.y = CANVASTAG.height + this.height;
+    }
+  };
+  this.checkCollisions = function (object, index) {
+    if (
+      this.x < object.x + object.width &&
+      this.x + this.width > object.x &&
+      this.y < object.y + object.height &&
+      this.y + this.height > object.y
+    ) {
+      console.log("Bati: ", asteroidList[index]);
+      asteroidList[index].canRender = false;
+    }
+  };
+  this.setPositionAndDirection = function () {
+    const xDirection = Math.floor(Math.random() * 2);
+    const yDirection = Math.floor(Math.random() * 2);
+    this.moveAngle = Math.floor(Math.random() * 2);
+    this.x = Math.floor(Math.random() * CANVASTAG.width) * xDirection;
+    this.y = Math.floor(Math.random() * CANVASTAG.height) * yDirection;
+    this.speedX = xDirection ? this.speedX * -1 : this.speedX;
+    this.speedY = yDirection ? this.speedY * -1 : this.speedY;
+  };
+}
+
+function render() {
+  clearScreen();
+  spaceShip.update();
+  spaceShip.draw();
+  asteroidList.forEach((asteroid, index) => {
+    if (asteroid.canRender) {
+      asteroid.draw();
+      asteroid.move();
+      asteroid.checkCollisions(spaceShip, index);
+    }
+  });
+}
+
+gameLoop();
+inputProcess();
